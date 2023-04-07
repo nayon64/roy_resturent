ae11317dc51ddf45c4db8b43c12af6f318f63753
Nayon <nayonroy64@gmail.com>
2023-04-07 13:57:51 +0600

    create next.js project and set up

---
+++ pages/about/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const about = () => {
+	return (
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>About page</h1>
+    </div>
+  );
+};
+
+export default about;
